4009ac1875a4faa528caa9448875a8de59d83a71
yukii <lichtestv@gmail.com>
2026-08-18 11:34:24 +0200

    IDs: Added StringId and AstNodeId

--- src/ids.h
+++ src/ids.h
@@ -3,8 +3,12 @@
 
 #include "utils/types.h"
 
+typedef u32 AstNodeId;
 typedef u32 FileId;
+typedef u32 StringId;
 
+static constexpr u32 AST_NODE_ID_NONE = U32_MAX;
 static constexpr u32 FILE_ID_NONE = U32_MAX;
+static constexpr u32 STRING_ID_NONE = U32_MAX;
 
 #endif // !LILY_IDS_H
