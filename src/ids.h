01c81acfc4bb610dd2baac967a2395a84227f586
yukii <lichtestv@gmail.com>
2026-08-18 16:10:00 +0200

    IDs: Added TypeId

--- src/ids.h
+++ src/ids.h
@@ -7,10 +7,12 @@ typedef u32 AstNodeId;
 typedef u32 FileId;
 typedef u32 NamespaceId;
 typedef u32 StringId;
+typedef u32 TypeId;
 
 static constexpr u32 AST_NODE_ID_NONE = U32_MAX;
 static constexpr u32 FILE_ID_NONE = U32_MAX;
 static constexpr u32 NAMESPACE_ID_NONE = U32_MAX;
 static constexpr u32 STRING_ID_NONE = U32_MAX;
+static constexpr u32 TYPE_ID_NONE = U32_MAX;
 
 #endif // !LILY_IDS_H
